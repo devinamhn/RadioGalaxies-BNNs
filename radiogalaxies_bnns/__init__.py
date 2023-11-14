43b2f105956063b182e5b835b3d6c8f950e7c2ec
Devina Mohan <devinamhn@gmail.com>
2023-11-14 18:49:35 +0000

    init.py

--- radiogalaxies_bnns/__init__.py
+++ radiogalaxies_bnns/__init__.py
@@ -1,7 +1,7 @@
-from inference.models import MLP, LeNet, LeNetDrop
-from inference.datamodules import MiraBestDataModule, MNISTDataModule
+# from inference.models import MLP, LeNet, LeNetDrop
+# from inference.datamodules import MiraBestDataModule, MNISTDataModule
 
-from inference.datamodules import testloader_mb_uncert
+# from inference.datamodules import testloader_mb_uncert
 
-from inference.utils import calibration_test
-from inference.utils import get_logits, get_logits_mlp
+# from inference.utils import calibration_test
+# from inference.utils import get_logits, get_logits_mlp
