cbfaa54d120f353c67d41e5e97d3a53252c5b925
Devina Mohan <devinamhn@gmail.com>
2024-04-30 16:34:43 +0100

    deleted emsembles folder

--- radiogalaxies_bnns/inference/hmc/temp_test.sh
+++ radiogalaxies_bnns/inference/hmc/temp_test.sh
@@ -5,8 +5,8 @@
 #SBATCH --time=10-23
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
-#SBATCH --output=/share/nas2/dmohan/mcmc/hamilt/logs/leapfrog/out-slurm_%A_%a.out
-#SBATCH --array=1-6
+#SBATCH --output=/share/nas2/dmohan/RadioGalaxies-BNNs/radiogalaxies_bnns/logs/inits/out-slurm_%A_%a.out
+#SBATCH --array=1-5
 #SBATCH --cpus-per-task=17
 #SBATCH --exclude=compute-0-4
 
@@ -18,7 +18,7 @@ nvidia-smi
 echo ">>>start task array $SLURM_ARRAY_TASK_ID"
 echo "Running on:"
 hostname
-source /share/nas2/dmohan/mcmc/hamilt/venv/bin/activate
+source /share/nas2/dmohan/RadioGalaxies-BNNs/venv/bin/activate
 echo ">>>training"
-python /share/nas2/dmohan/mcmc/hamilt/temp.py $SLURM_ARRAY_TASK_ID
+python /share/nas2/dmohan/RadioGalaxies-BNNs/radiogalaxies_bnns/inference/hmc/temp.py $SLURM_ARRAY_TASK_ID
 
