411c068b7a5a1757730e7244b65ce38e1290741a
Devina Mohan <devinamhn@gmail.com>
2024-04-30 15:36:18 +0100

    added array

--- radiogalaxies_bnns/inference/dropout/train_dropout.sh
+++ radiogalaxies_bnns/inference/dropout/train_dropout.sh
@@ -5,14 +5,17 @@
 #SBATCH --time=10-23
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
+#SBATCH --array=1-10
 #SBATCH --output=/share/nas2/dmohan/RadioGalaxies-BNNs/radiogalaxies_bnns/logs/dropout/out-slurm_%j.out
 
 
 pwd;
 
 nvidia-smi 
-echo ">>>start"
+echo ">>>start task array $SLURM_ARRAY_TASK_ID"
+echo "Running on:"
+hostname
 source /share/nas2/dmohan/RadioGalaxies-BNNs/venv/bin/activate
 echo ">>>training"
-python /share/nas2/dmohan/RadioGalaxies-BNNs/radiogalaxies_bnns/inference/dropout/dropout_train.py
+python /share/nas2/dmohan/RadioGalaxies-BNNs/radiogalaxies_bnns/inference/dropout/dropout_train.py $SLURM_ARRAY_TASK_ID
 
