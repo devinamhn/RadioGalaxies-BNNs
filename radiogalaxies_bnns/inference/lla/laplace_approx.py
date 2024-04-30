3c01795e66d103baa788eae5d3899bd8b06e0f21
Devina Mohan <devinamhn@gmail.com>
2024-04-30 15:28:20 +0100

    code for fitting lla for 10 experimental runs; calculate test err and uce

--- radiogalaxies_bnns/inference/lla/laplace_approx.py
+++ radiogalaxies_bnns/inference/lla/laplace_approx.py
@@ -1,10 +1,10 @@
 import numpy as np
 import torch
 import torch.distributions as dists
-from inference.models import LeNetDrop
-import utils
-from inference.datamodules import MNISTDataModule, MiraBestDataModule, testloader_mb_uncert
-from uncertainty import entropy_MI, overlapping, GMM_logits, calibration
+from radiogalaxies_bnns.inference.models import LeNet
+import radiogalaxies_bnns.inference.utils as utils
+from radiogalaxies_bnns.inference.datamodules import MNISTDataModule, MiraBestDataModule, testloader_mb_uncert
+from radiogalaxies_bnns.eval.uncertainty.uncertainty import entropy_MI, overlapping, GMM_logits, calibration
 
 from laplace import Laplace
 from netcal.metrics import ECE
@@ -15,6 +15,7 @@ def credible_interval(samples, credibility):
 
     samples values and indices
     '''
+    mean_samples = samples.mean()
     sorted_samples = np.sort(samples)
     lower_bound = 0.5 * (1 - credibility)
     upper_bound = 0.5 * (1 + credibility)
@@ -22,25 +23,30 @@ def credible_interval(samples, credibility):
     index_lower = int(np.round(len(samples) * lower_bound))
     index_upper = int(np.round(len(samples) * upper_bound))
 
-    return sorted_samples, index_lower, index_upper
+    return sorted_samples, index_lower, index_upper, mean_samples
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-config_dict, config = utils.parse_config('config_mb_dropout.txt')
-seed = 123 #config['training']['seed']
+config_dict, config = utils.parse_config('/share/nas2/dmohan/RadioGalaxies-BNNs/radiogalaxies_bnns/inference/nonBayesianCNN/config_cnn.txt')
+seed = 122 #config['training']['seed']
 torch.manual_seed(seed)
 
 datamodule = MiraBestDataModule(config_dict, hmc=False)
 train_loader = datamodule.train_dataloader()
 validation_loader = datamodule.val_dataloader()
 test_loader = datamodule.test_dataloader()
+path_out = config_dict['output']['path_out']
+
+print('TEST DATASET')
+print(config_dict['output']['test_data'])
+
 targets = torch.cat([y for x, y in test_loader], dim=0).to(device)#.numpy()
 
 
-#Non-Bayesian CNN if dropout_rate = 0
-model = LeNetDrop(in_channels = 1, output_size =  2, dropout_rate = 0 ).to(device) #MLP(150, 200, 10)
+# #Non-Bayesian CNN if dropout_rate = 0
+# model = LeNet(in_channels = 1, output_size =  2).to(device)
 
-
-model.load_state_dict(torch.load('./dropout/mlp/model', map_location=torch.device('cpu') ))
+# model_path = path_out+ str(8) +'/model'
+# model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu') ))
 
 @torch.no_grad()
 def predict(dataloader, model, laplace=False):
@@ -54,28 +60,28 @@ def predict(dataloader, model, laplace=False):
 
     return torch.cat(py)#.cpu().numpy()
 
-probs_map = predict(test_loader, model, laplace=False)
-print((probs_map.argmax(-1) == targets)*1)
-acc_map = ((probs_map.argmax(-1) == targets)*1).float().mean()
-ece_map = ECE(bins=8).measure(probs_map.cpu().numpy(), targets.cpu().numpy())
-nll_map = -dists.Categorical(probs_map).log_prob(targets).mean()
-print(f'[MAP] Acc.: {acc_map:.1%}; ECE: {ece_map:.1%}; NLL: {nll_map:.3}')
+# probs_map = predict(test_loader, model, laplace=False)
+# print((probs_map.argmax(-1) == targets)*1)
+# acc_map = ((probs_map.argmax(-1) == targets)*1).float().mean()
+# ece_map = ECE(bins=8).measure(probs_map.cpu().numpy(), targets.cpu().numpy())
+# nll_map = -dists.Categorical(probs_map).log_prob(targets).mean()
+# print(f'[MAP] Acc.: {acc_map:.1%}; ECE: {ece_map:.1%}; NLL: {nll_map:.3}')
 
 
 # Laplace
-la = Laplace(model, 'classification',
-             subset_of_weights= 'last_layer',
-             hessian_structure='diag')
-la.fit(train_loader)
-la.optimize_prior_precision(pred_type = 'nn', method='marglik', verbose = True)
+# la = Laplace(model, 'classification',
+#              subset_of_weights= 'last_layer',
+#              hessian_structure='diag') # {diag, kron, full}
+# la.fit(train_loader)
+# la.optimize_prior_precision(pred_type = 'nn', method='marglik', verbose = True)
 
 
-posterior_precision = la.posterior_precision
-posterior_std = torch.sqrt(1/posterior_precision)
-print(la.sample(200)[:, 0:168].shape)
+# posterior_precision = la.posterior_precision
+# posterior_std = torch.sqrt(1/posterior_precision)
+# print(la.sample(200)[:, 0:168].shape)
 
-samples  = la.sample(200)[:, 0:168]
-torch.save(samples, './results/laplace/lla_samples.pt')
+# samples  = la.sample(200)[:, 0:168]
+# torch.save(samples, './results/laplace/lla_samples.pt')
 
 # torch.save(la.sample)
 # print(posterior_std)
@@ -83,193 +89,176 @@ torch.save(samples, './results/laplace/lla_samples.pt')
 # torch.distributions.Normal(
 
 
-exit()
-probs_laplace = predict(test_loader, la, laplace=True)
-acc_laplace = ((probs_laplace.argmax(-1) == targets)*1).float().mean()
-ece_laplace = ECE(bins=8).measure(probs_laplace.cpu().numpy(), targets.cpu().numpy())
-nll_laplace = -dists.Categorical(probs_laplace).log_prob(targets).mean()
 
-print(f'[Diag Laplace] Acc.: {acc_laplace:.1%}; ECE: {ece_laplace:.1%}; NLL: {nll_laplace:.3}')
+# probs_laplace = predict(test_loader, la, laplace=True)
+# acc_laplace = ((probs_laplace.argmax(-1) == targets)*1).float().mean()
+# ece_laplace = ECE(bins=8).measure(probs_laplace.cpu().numpy(), targets.cpu().numpy())
+# nll_laplace = -dists.Categorical(probs_laplace).log_prob(targets).mean()
 
-
-x_data, y_data = data
+# print(f'[Diag Laplace] Acc.: {acc_laplace:.1%}; ECE: {ece_laplace:.1%}; NLL: {nll_laplace:.3}')
+# print(f'[Kron] Acc.: {acc_laplace:.1%}; ECE: {ece_laplace:.1%}; NLL: {nll_laplace:.3}')
 
 
-# test_loader, test_data1, data_type, test_data = testloader_mb_uncert(config_dict['output']['test_data'], config_dict['data']['datadir'])
-
-for i, (x_test, y_test) in enumerate(test_loader):
-    x_test, y_test = x_test.to(device), y_test.to(device)
-
-# print(len(y_test))
-
-# for i, (x_test, y_test) in enumerate(test_loader):
-#     x_test, y_test = x_test.to(device), y_test.to(device)
-#     print(x_test.shape)
-#     pred = la.predictive_samples(x_test.cuda(), pred_type = 'nn', n_samples = 200 )
-
-#     print(pred.shape)
-#     # print(pred)
-#     exit()
+# x_data, y_data = data
 
 
-###################################################################################
+test_loader, test_data1, data_type, test_data = testloader_mb_uncert(config_dict['output']['test_data'], config_dict['data']['datadir'])
+mean_expected_error_arr, std_expected_error_arr, uce_pe_arr, uce_mi_arr, uce_ae_arr = np.zeros(10), np.zeros(10), np.zeros(10), np.zeros(10), np.zeros(10)
 
-exit()
-
-print('Dropout')
-#Non-Bayesian CNN if dropout_rate = 0
-model = LeNetDrop(in_channels = 1, output_size =  2, dropout_rate = 0.5).to(device) #MLP(150, 200, 10)
-
-
-model.load_state_dict(torch.load('./dropout/exp3/model', map_location=torch.device('cpu') ))
-
-probs_map = predict(test_loader, model, laplace=False)
-print((probs_map.argmax(-1) == targets)*1)
-acc_map = ((probs_map.argmax(-1) == targets)*1).float().mean()
-ece_map = ECE(bins=8).measure(probs_map.cpu().numpy(), targets.cpu().numpy())
-nll_map = -dists.Categorical(probs_map).log_prob(targets).mean()
-print(f'[MAP] Acc.: {acc_map:.1%}; ECE: {ece_map:.1%}; NLL: {nll_map:.3}')
-
-
-# Laplace
-la = Laplace(model, 'classification',
-             subset_of_weights= 'last_layer',
-             hessian_structure='diag')
-la.fit(train_loader)
-la.optimize_prior_precision(method='marglik')
-
-
-probs_laplace = predict(test_loader, la, laplace=True)
-acc_laplace = ((probs_laplace.argmax(-1) == targets)*1).float().mean()
-ece_laplace = ECE(bins=8).measure(probs_laplace.cpu().numpy(), targets.cpu().numpy())
-nll_laplace = -dists.Categorical(probs_laplace).log_prob(targets).mean()
-
-print(f'[Diag Laplace] Acc.: {acc_laplace:.1%}; ECE: {ece_laplace:.1%}; NLL: {nll_laplace:.3}')
 
 
 ############################
 # UCE Calculation
 ############################
-num_samples = 200
-error_all = []
-entropy_all = []
-mi_all = []
-aleat_all =[]
+for n_ensemble in np.arange(0, 10, 1):
+    print('Ensemble', n_ensemble)
+
+    #Non-Bayesian CNN if dropout_rate = 0
+    model = LeNet(in_channels = 1, output_size =  2).to(device)
+
+    model_path = path_out+ str(n_ensemble+1) +'/model'
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu') ))
+
+    # Laplace
+    la = Laplace(model, 'classification',
+                subset_of_weights= 'last_layer',
+                hessian_structure='diag') # {diag, kron, full}
+    la.fit(train_loader)
+    la.optimize_prior_precision(pred_type = 'nn', method='marglik', verbose = True)
+
+    probs_laplace = predict(test_loader, la, laplace=True)
+    acc_laplace = ((probs_laplace.argmax(-1) == targets)*1).float().mean()
+    ece_laplace = ECE(bins=8).measure(probs_laplace.cpu().numpy(), targets.cpu().numpy())
+    nll_laplace = -dists.Categorical(probs_laplace).log_prob(targets).mean()
+
+    print(f'[Diag Laplace] Acc.: {acc_laplace:.1%}; ECE: {ece_laplace:.1%}; NLL: {nll_laplace:.3}')
+
+
+    num_samples = 200
+    error_all = []
+    entropy_all = []
+    mi_all = []
+    aleat_all =[]
+    avg_error_mean = []
+
+    fr1 = 0
+    fr2 = 0
+
+    indices = np.arange(0, len(test_data), 1)
+    # print(indices)
+
+    #for each sample in the test set:
+    for index in indices:
+
+
+        x_test = torch.unsqueeze(torch.tensor(test_data[index][0]),0)
+        y_test = torch.unsqueeze(torch.tensor(test_data[index][1]),0)
+
+
+        target = y_test.detach().numpy().flatten()[0]
+
+        if(target == 0):
+            fr1+= 1
+        elif(target==1):
+            fr2+= 1
+
+        softmax_values = la.predictive_samples(x_test.cuda(), pred_type = 'nn', n_samples = num_samples )
+        logits = la.predictive_samples(x_test.cuda(), pred_type = 'nn', n_samples = num_samples, return_logits = True )
+        softmax_ = softmax_values.cpu()
+        logits_ = logits.cpu()
+
     
-fr1 = 0
-fr2 = 0
-
-indices = np.arange(0, len(test_data), 1)
-print(indices)
-
-#for each sample in the test set:
-for index in indices:
-
-    # x_test = x_test[index] #torch.unsqueeze(x_test[index])
-    # y_test = y_test[index] #torch.unsqueeze(x_test[index])
-    x_test = torch.unsqueeze(torch.tensor(test_data[index][0]),0)
-    y_test = torch.unsqueeze(torch.tensor(test_data[index][1]),0)
-
-    # x_test[0]
-    # print(x_test, y_test)
-
-    # target = y_test[index].detach().numpy()
-    target = y_test.detach().numpy().flatten()[0]
-
-    if(target == 0):
-        fr1+= 1
-    elif(target==1):
-        fr2+= 1
-
-    softmax_values = la.predictive_samples(x_test.cuda(), pred_type = 'nn', n_samples = num_samples )
-    softmax_ = softmax_values.cpu()
-
-    pred = softmax_.mean(dim=1).argmax(dim=-1).numpy().flatten()
-    # pred1 = pred_list.mean(dim=1).argmax(dim=-1)
-
-    y_test_all = np.tile(y_test.detach().numpy().flatten()[0], num_samples)
-
-    # print(pred)
-    # print(pred1)
-    # print(y_test_all)
-
-    # print(pred != y_test_all)
-
-    errors =  np.mean((pred != y_test_all).astype('uint8'))
-    # print(errors)
-
-    softmax = np.array(softmax_).reshape((num_samples, 2))
-    logits = pred #np.array(pred) 
-
-    mean_logits = np.mean(logits,axis=0)
-    var_logits = np.std(logits,axis=0)
-    entropy, mutual_info, entropy_singlepass = entropy_MI(softmax, samples_iter= num_samples)
-  
-
-    error_all.append(errors)
-    entropy_all.append(entropy/np.log(2))    
-    mi_all.append(mutual_info/np.log(2))
-    aleat_all.append(entropy_singlepass/np.log(2))
-
-
-print(error_all)
-print(entropy_all)
-
-path = './dropout/mlp/' 
-
-fr1_start = 0 #{0}
-fr1_end = fr1 #{49, 68}
-fr2_start = fr1 #49, 68
-fr2_end = len(indices) #len(val_indices) #{104, 145}
+        pred = softmax_.argmax(dim=-1).numpy().flatten()
     
-# print(fr1_start, fr1_end, fr2_start, fr2_end)
+
+        y_test_all = np.tile(y_test.detach().numpy().flatten()[0], num_samples)
+
+        
+        softmax = np.array(softmax_.detach().numpy())
+        y_logits = np.array(logits_.detach().numpy())
+        
+        sorted_softmax_values, lower_index, upper_index, mean_samples = credible_interval(softmax[ :, :, 0].flatten(), 0.64)
+        
+        sorted_softmax_values_fr1 = sorted_softmax_values[lower_index:upper_index]
+        sorted_softmax_values_fr2 = 1 - sorted_softmax_values[lower_index:upper_index]
+
     
-#print(error_all)
-#entropy, mutual_info, entropy_singlepass = entropy_MI(softmax, samples_iter)
-n_bins = 8
+        softmax_mean = np.vstack((mean_samples, 1-mean_samples)).T
+        softmax_credible = np.vstack((sorted_softmax_values_fr1, sorted_softmax_values_fr2)).T   
+
+
+        
+        entropy, mutual_info, entropy_singlepass = entropy_MI(softmax_credible, 
+                                                        samples_iter= len(softmax_credible[:,0]))
+
+        pred = np.argmax(softmax_credible, axis = 1)
+
+        y_test_all = np.tile(y_test, len(softmax_credible[:,0]))
+
+        errors =  np.mean((pred != y_test_all).astype('uint8'))
+
     
-uce  = calibration(path, np.array(error_all), np.array(entropy_all), n_bins, x_label = 'predictive entropy')
-print("Predictive Entropy")
-print("uce = ", np.round(uce, 2))
-uce_0  = calibration(path, np.array(error_all[fr1_start:fr1_end]), np.array(entropy_all[fr1_start:fr1_end]), n_bins, x_label = 'predictive entropy') 
-print("UCE FRI= ", np.round(uce_0, 2))
-uce_1  = calibration(path, np.array(error_all[fr2_start:fr2_end]), np.array(entropy_all[fr2_start:fr2_end]), n_bins, x_label = 'predictive entropy')
-print("UCE FRII = ", np.round(uce_1, 2))
-cUCE = (uce_0 + uce_1)/2 
-print("cUCE=", np.round(cUCE, 2))
-    
-#max_mi = np.amax(np.array(mi_all))
-#print("max mi",max_mi)
-#mi_all = mi_all/max_mi
-#print(mi_all)
-#print("max mi",np.amax(mi_all))
-uce  = calibration(path, np.array(error_all), np.array(mi_all), n_bins, x_label = 'mutual information')
-print("Mutual Information")
-print("uce = ", np.round(uce, 2))
-uce_0  = calibration(path, np.array(error_all[fr1_start:fr1_end]), np.array(mi_all[fr1_start:fr1_end]), n_bins, x_label = 'mutual information') 
-print("UCE FRI= ", np.round(uce_0, 2))
-uce_1  = calibration(path, np.array(error_all[fr2_start:fr2_end]), np.array(mi_all[fr2_start:fr2_end]), n_bins, x_label = 'mutual information')
-print("UCE FRII = ", np.round(uce_1, 2))
-cUCE = (uce_0 + uce_1)/2 
-print("cUCE=", np.round(cUCE,2))  
+        pred_mean = np.argmax(softmax_mean, axis = 1)
+        error_mean = (pred_mean != target)*1
+
+        error_all.append(errors)
+        avg_error_mean.append(error_mean)
+        entropy_all.append(entropy/np.log(2))    
+        mi_all.append(mutual_info/np.log(2))
+        aleat_all.append(entropy_singlepass/np.log(2))
 
 
-uce  = calibration(path, np.array(error_all), np.array(aleat_all), n_bins, x_label = 'average entropy')
-print("Average Entropy")
-print("uce = ", np.round(uce, 2))
-uce_0  = calibration(path, np.array(error_all[fr1_start:fr1_end]), np.array(aleat_all[fr1_start:fr1_end]), n_bins, x_label = 'average entropy') 
-print("UCE FRI= ",np.round(uce_0, 2))
-uce_1  = calibration(path, np.array(error_all[fr2_start:fr2_end]), np.array(aleat_all[fr2_start:fr2_end]), n_bins, x_label = 'average entropy')
-print("UCE FRII = ", np.round(uce_1, 2))
-cUCE = (uce_0 + uce_1)/2 
-print("cUCE=", np.round(cUCE, 2))  
+
+    path = './'
+
+    fr1_start = 0 #{0}
+    fr1_end = fr1 #{49, 68}
+    fr2_start = fr1 #49, 68
+    fr2_end = len(indices) #len(val_indices) #{104, 145}
+        
+    n_bins = 8
+        
+    uce_pe  = calibration(path, np.array(error_all), 
+                        np.array(entropy_all), n_bins, x_label = 'predictive entropy')
+    uce_mi  = calibration(path, np.array(error_all), 
+                        np.array(mi_all), n_bins, x_label = 'mutual information')
+    uce_ae  = calibration(path, np.array(error_all), 
+                        np.array(aleat_all), n_bins, x_label = 'average entropy')
 
 
-print("mean and std of error")
-print(error_all)
-print(np.mean(error_all)*100)
-print(np.std(error_all))
 
+    # print("mean and std of error")
+    # print(error_all)
+    # print(np.mean(error_all)*100)
+    # print(np.std(error_all))
+
+    # print("Average of expected error")
+    # print((np.array(avg_error_mean)).mean()*100)
+    # print((np.array(avg_error_mean)).std())
+        
+    mean_expected_error = np.array(avg_error_mean).mean()*100 
+    std_expected_error = np.array(avg_error_mean).std()
+
+    # print('Predictive entropy', uce_pe)
+    # print('Mututal info', uce_mi)
+    # print('Aleatoric uncertainty', uce_ae)
+    mean_expected_error_arr[n_ensemble], std_expected_error_arr[n_ensemble], uce_pe_arr[n_ensemble], uce_mi_arr[n_ensemble], uce_ae_arr[n_ensemble] = mean_expected_error, std_expected_error, uce_pe, uce_mi, uce_ae
+
+print("Mean of error over seeds", mean_expected_error_arr.mean())
+print("Std of error over seeds", mean_expected_error_arr.std())
+
+print("Mean of std over seeds", std_expected_error_arr.mean())
+print("Std of std over seeds", std_expected_error_arr.std())
+
+
+print('Mean of uce pe', uce_pe_arr.mean())
+print('Std of uce pe', uce_pe_arr.std())
+
+print('Mean of uce mi', uce_mi_arr.mean())
+print('Std of uce mi', uce_mi_arr.std())
+
+print('Mean of uce ae', uce_ae_arr.mean())
+print('Std of uce ae', uce_ae_arr.std())
 
 exit()
 
